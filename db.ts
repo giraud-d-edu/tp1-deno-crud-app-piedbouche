f5d33de4d6fe4b610a22610f437aef40ba92393f
Hippot <hippot02@gmail.com>
2025-03-04 09:45:52 +0100

    Ajout BDD

---
+++ db.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "npm:mongodb@5.6.0";
+
+const MONGODB_URI = "mongodb+srv://corehippot:CvFRfNBmDF2M3BP1@cluster0.1mhsrpr.mongodb.net/"
+const DB_NAME = "films_DB";
+
+if (!MONGODB_URI) {
+  console.error("MONGODB_URI is not set");
+  Deno.exit(1);
+}
+
+const client = new MongoClient(MONGODB_URI);
+
+try {
+  await client.connect();
+  await client.db("admin").command({ ping: 1 });
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+  Deno.exit(1);
+}
+
+const db = client.db(DB_NAME);
+const todos = db.collection("todos");
+
+export { db, todos };
--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router.ts";
+import { db } from "./db.ts"; // Importez la base de données
 
 const app = new Application();
+
+// Exemple d'utilisation de la base de données pour vérifier la connexion
+try {
+  await db.command({ ping: 1 });
+  console.log("Successfully connected to the database");
+} catch (error) {
+  console.error("Error connecting to the database:", error);
+}
 
 app.use(router.routes());
 app.use(router.allowedMethods());
